779e9c16169a4c96acbd46f09f81dbff1a6c244d
Dilshod Qulmirzayev <dilshodqulmirzayev21@gmail.com>
2023-09-03 12:24:57 +0500

    Created data modul for collectionVeiw

---
+++ Coder-swag/Coder-swag/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  Coder-swag
+//
+//  Created by MacBook Pro on 03/09/23.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
--- Coder-swag/Coder-swag/Services/DataServices.swift
+++ Coder-swag/Coder-swag/Services/DataServices.swift
@@ -17,6 +17,66 @@ class DataService {
     Category(categoryTitle: "HATS", imageName: "hats.png"),
     Category(categoryTitle: "DIGITAL", imageName: "digital.png")
     ]
+    
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", price: "@18", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo Hat Black", price: "$22", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Hat White", price: "$43", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo Snapback", price: "$23", imageName: "hat04.png")
+    ]
+    
+    private let hoodies = [
+      Product(title: "Devslopes Logo Hoodie Grey", price: "$23", imageName: "hoodie01.png"),
+      Product(title: "Devslopes Logo Hoodie Red", price: "$25", imageName: "hoodie02.png"),
+      Product(title: "Devslopes  Hoodie Grey", price: "$43", imageName: "hoodie03.png"),
+      Product(title: "Devslopes  Hoodie Black", price: "$33", imageName: "hoodie04.png"),
+    ]
+    
+    private let shirts = [
+     
+        Product(title: "Devslopes Logo Shirt Black", price: "$18", imageName: "shirt01.png"),
+        Product(title: "Devslopes badge Shirt Light Grey", price: "$28", imageName: "shirt02.png"),
+        Product(title: "Devslopes Logo Shirt Red", price: "$34", imageName: "shirt03.png"),
+        Product(title: "Hustle Delegate Gray", price: "$25", imageName: "shirt04.png"),
+        Product(title: "Kickflip Studios Black", price: "$25", imageName: "shirt05.png"),
+    ]
+    
+    private let digitalGoods = [Product]()
+    
+    func getProducts(forCategoryTitle title: String) -> [Product] {
+        
+        switch title{
+        case "SHIRTS":
+           return getShirts()
+        case "HATS":
+          return   getHats()
+        case "HOODIES":
+         return   getHoodies()
+        case "DIGITAL":
+          return  getDeigitalGoods()
+        default:
+           return getShirts()
+        }
+        
+    }
+    
+    func getHats() -> [Product] {
+        return hats
+    }
+    
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    
+    func getShirts() -> [Product] {
+        return shirts
+    }
+    
+    func getDeigitalGoods() -> [Product] {
+        return digitalGoods
+    }
+    
+    
     func getCategories() -> [Category] {
         return catecories
     }
