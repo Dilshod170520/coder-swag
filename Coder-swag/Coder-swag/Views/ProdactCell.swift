079436a234ab6611bae238f40eca0b47a88db25b
Dilshod Qulmirzayev <dilshodqulmirzayev21@gmail.com>
2023-09-03 11:55:16 +0500

    added collectionVeiw and cell

---
+++ Coder-swag/Coder-swag/Views/ProdactCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProdactCell.swift
+//  Coder-swag
+//
+//  Created by MacBook Pro on 03/09/23.
+//
+
+import UIKit
+
+class ProdactCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
